0a345535a325a729122f5119c173759324b04e4b
Michael Sumner <mdsumner@gmail.com>
2020-03-25 16:06:07 +1100

    check more EPSG

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,4 +5,10 @@ library(raster)
 .mesh3d <- quadmesh(crop(worldll, extent(100, 150, -60, -30)))
 data(wrld_simpl, package = "maptools")
 .sc <- silicate::SC(subset(wrld_simpl, NAME %in% c("Australia", "New Zealand")))
-usethis::use_data(.sc, .mesh3d, internal = TRUE)
+
+## I will surely pay for this
+epsg <- rgdal::make_EPSG()
+idx <- grep("\\+proj\\=longlat", epsg$prj4)
+.epsg_code <- sort(unique(epsg$code[idx]))
+
+usethis::use_data(.sc, .mesh3d, .epsg_code, internal = TRUE)
